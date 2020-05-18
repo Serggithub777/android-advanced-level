dffe39fc5b215bcc9f3d6e10e0cd08246e46a1cc
SergSerg <sergio.ne@inbox.ru>
2020-05-18 22:27:29 +0600

    Создал Контекстное меню в списке в HomeFragmnet

--- Lesson_1/Menu/app/src/main/java/com/example/menu/ListAdapter.java
+++ Lesson_1/Menu/app/src/main/java/com/example/menu/ListAdapter.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     private List<String> data;
     private Fragment fragment;
+    private int menuPosition;
 
     public ListAdapter(List<String> data, Fragment fragment) {
         this. data = data;
@@ -28,10 +29,18 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
         return new ViewHolder(view);
     }
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
 // Заполнение элементов холдера
         TextView textElement = holder.getTextElement() ;
         textElement.setText(data.get(position) ) ;
+        // Определяем текущую позицию в списке
+        textElement. setOnLongClickListener(new View. OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                menuPosition = position;
+                return false;
+            }
+        } ) ;
 // Так регистрируется контекстное меню
         if (fragment != null) {
             fragment.registerForContextMenu(textElement) ;
@@ -43,25 +52,30 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     // endregion
     // region Изменение списка
     // Добавить элемент в список
-    void addItem(String element) {
+  public  void addItem(String element) {
         data.add(element) ;
         notifyItemInserted(data.size() -1) ;
     }
     // Заменить элемент в списке
-    void updateItem(String element, int position) {
+  public  void updateItem(String element, int position) {
         data.set(position, element) ;
         notifyItemChanged(position) ;
     }
     // Удалить элемент из списка
-    void removeItem(int position) {
+   public void removeItem(int position) {
         data.remove(position) ;
         notifyItemRemoved(position) ;
     }
     // Очистить список
-    void clearItems() {
+   public void clearItems() {
         data.clear() ;
         notifyDataSetChanged() ;
     }
+
+   public int getMenuPosition() {
+        return menuPosition;
+    }
+
     // endregion
     public class ViewHolder extends RecyclerView. ViewHolder {
         private TextView textElement;
--- Lesson_1/Menu/app/src/main/java/com/example/menu/ui/home/HomeFragment.java
+++ Lesson_1/Menu/app/src/main/java/com/example/menu/ui/home/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.menu.ui.home;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -50,6 +53,33 @@ public class HomeFragment extends Fragment {
             list.add(String.format("Element %d", i));
         }
         return list;
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu. ContextMenuInfo menuInfo) {
+        super. onCreateContextMenu(menu, v, menuInfo) ;
+        MenuInflater inflater = requireActivity().getMenuInflater();
+        inflater.inflate(R.menu.context_menu, menu) ;
+    }@
+            Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        ContextMenu.ContextMenuInfo menuInfo = item.getMenuInfo() ;
+        int id = item.getItemId() ;
+        switch (id) {
+            case R.id.add_context:
+                adapter.addItem(String.format("New element %d", adapter.getItemCount()));
+                return true;
+            case R.id.update_context:
+                adapter.updateItem(String. format("Updated element %d",
+                        adapter.getMenuPosition() ) , adapter.getMenuPosition() ) ;
+                return true;
+            case R.id.remove_context:
+                adapter.removeItem(adapter.getMenuPosition());
+                return true;
+            case R.id.clear_context:
+                adapter.clearItems();
+                return true;
+        }return super.onContextItemSelected(item);
     }
 }
 
