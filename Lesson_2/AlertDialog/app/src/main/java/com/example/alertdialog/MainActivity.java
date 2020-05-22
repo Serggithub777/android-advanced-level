29434050bc48c2a896cd0384fa00bc38a468b87a
SergSerg <sergio.ne@inbox.ru>
2020-05-22 21:59:39 +0600

    Создал проект Alert Dialog в Lesson 2

---
+++ Lesson_2/AlertDialog/app/src/main/java/com/example/alertdialog/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.alertdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button alert1 = findViewById(R.id.alertDialog1);
+
+    }
+}
